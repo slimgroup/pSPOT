c096f8af3a6e1b671a3654fbaac8e52b456c80e0
Nameet Kumar <nkumar@eos.ubc.ca>
2010-10-21 19:44:31 -0700

    current progress of oppSpot, incomplete

---
+++ @oppSpot/oppSpot.m
@@ -0,0 +1,60 @@
+classdef (InferiorClasses = {?opSpot}) oppSpot < opSpot
+   %oppSpot pSpot operator super class.
+   %
+   
+   
+   
+   
+   
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   % Properties
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   properties
+      
+   end %properties
+   
+   
+   
+   
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   % Public methods
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   methods
+      
+      function op = oppSpot(type,m,n)
+         %oppSpot  Constructor.
+         op = op@opSpot(type,m,n);
+      end
+      
+   end %methods - public
+   
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   % Protected methods
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   methods( Access = protected )
+      
+      function x = applyMultiply(op,x,mode)
+         op.counter.plus1(mode);
+         x = op.multiply(x,mode);
+         
+         
+         
+      end
+      
+      function x = applyDivide(op,x,mode)
+         x = op.divide(op,x,mode);
+      end
+      
+      % Signature of external protected functions
+      x = divide(op,x,mode);
+      
+   end %methods - protected
+   
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   % Abstract methods -- must be implemented by subclass.
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   methods( Abstract, Access = protected )
+       x = multiply(op,x,mode)
+   end % methods - abstract
+   
+end % classdef
