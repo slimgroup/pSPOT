9728172db2bde1e8db14527da578ca510552fdaf
Henryk Modzelewski <henryk_modzelewski@mac.com>
2011-02-24 11:39:53 -0800

    fixes to taper1D

--- +pWindow/taper1D.m
+++ +pWindow/taper1D.m
@@ -2,16 +2,17 @@ function [ t ] = taper1D( i, n, h )
 %taper1D 1D taper using sin()
 %   Detailed explanation goes here
 
-    if i<= n/2
+    if i< n/2
         x=i;
     else
         x=n-i+1;
     end
-    if x<=h+1;
+    b=2*h;
+    if x<b+1;
         t=sin(x*(pi/2.)/(2.*h+1.));
     else
         t=1;
     end
-    fprintf('%d %d %d\n',i,x,t);
+%    fprintf('%2d %2d %2d %f\n',b,i,x,t);
 end
 
