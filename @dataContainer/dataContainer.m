4c7852ebc77b80733bca82a621ccdb4715162b6e
Thomas <tlai@avo.eos.ubc.ca>
2011-03-07 14:18:34 -0800

    Renamed DataContainer to dataContainer

---
+++ @dataContainer/dataContainer.m
@@ -0,0 +1,37 @@
+classdef dataContainer < handle
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Properties
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    properties (SetAccess = private)
+        dims = [];          %original dimensions of data
+        collapsed = false;  %flag indicating if data is collapsed to a matrix
+        veced = false;      %flag indicating if data is collapsed to a vector
+        org = {};           %cell array describing which of the original dimensions is in each dimension of the data container
+        Data = []
+    end
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Methods
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    methods
+        function obj = dataContainer(data)
+            assert( isnumeric(data), 'DataContainer can only be created with numeric data' )
+            obj.Data = distributed(data);
+            obj.dims = size(data);
+            obj.org = num2cell( 1:ndims(data) );
+        end
+        
+        function d = Dims( obj )
+            d = obj.dims;
+        end
+        
+        function o = Org( obj )
+            o = obj.org;
+        end
+    end
+    
+    
+end
