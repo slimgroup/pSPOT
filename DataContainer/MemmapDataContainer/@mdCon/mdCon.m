4faedeb853f4776bd6abeb6d79cc1459cada76fd
Thomas <tlai@avo.eos.ubc.ca>
2011-03-24 11:35:06 -0700

    Moved mdCon directory into DataCon

--- DataContainer/MemmapDataContainer/@mdCon/mdCon.m
+++ DataContainer/MemmapDataContainer/@mdCon/mdCon.m
@@ -1,7 +1,7 @@
-classdef mdCon
+classdef mdCon < dataContainer
     %MDCON  Memmapfile Data Container class
     %
-    %   mdCon(FILENAME,PARAM1,VALUE1,...)
+    %   mdCon(FILENAME,SIZE,PARAM1,VALUE1,...)
     %
     %   Parameters:
     %   format - The precision of the data file. default 'double'
@@ -12,8 +12,8 @@ classdef mdCon
     properties (Access = protected)
         filename  = '';
         format    = 'double';
-        ifilename = '';
-        ofilename = '';
+        istemp    = false;
+        odims     = [];
         
     end
     
@@ -21,19 +21,25 @@ classdef mdCon
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %   Constructor
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        function x = mdCon(f,varargin)
+        function x = mdCon(f,s,varargin)
             
             % Check arguments size
             assert(rem(length(varargin), 2) == 0,...
                 'Param/value pairs must come in pairs.')
             
+            % Extract sizes
+            assert(isnumeric(s),'Sizes must be numeric')
+            dims = num2cell(s);
+            dims{end} = 0;
+            
+            % Construct in-core data container
+            x = x@dataContainer(zeros(dims{:}));
+            x.odims = s;
+            
             % Extract filename
             assert(ischar(f),'Filename must be a string')
             x.filename = f;
-            
-            % Setup default output filename
-            x.ofilename = strcat('output_',f);
-            
+                        
             % Parse param-value pairs
             for i = 1:2:length(varargin)
                 
@@ -52,6 +58,12 @@ classdef mdCon
             end                       
             
         end % constructor
+        
+        function delete(x)
+            if x.istemp
+               delete(x.filename); 
+            end
+        end % delete
         
     end % public methods
     
