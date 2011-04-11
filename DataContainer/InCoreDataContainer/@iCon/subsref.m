728fce9cc88248dda4e3935a46608425452ad33a
Thomas <tlai@avo.eos.ubc.ca>
2011-04-11 13:00:47 -0700

    Fixed subsref function call in datacon

--- DataContainer/InCoreDataContainer/@iCon/subsref.m
+++ DataContainer/InCoreDataContainer/@iCon/subsref.m
@@ -4,7 +4,7 @@ function varargout = subsref(x,s)
 %   X.<property> returns the value stored in that property: Currently the
 %   two properties that the user would be interested in is:
 %
-%   X(a,b,..) - where a,b,.. are indices returns the explicit 
+%   X(a,b,..) - where a,b,.. are indices returns the explicit
 %               elements stored within the data container as if it is a
 %               Matlab array. Actually this level of subreferencing is
 %               absolutely transparent. So don't expect a data container to
@@ -15,51 +15,54 @@ function varargout = subsref(x,s)
 %               the original, the copies and whatnot.
 %
 %   See also: vec, unvec, subsasgn
-   
-%   Copyright 2009, Ewout van den Berg and Michael P. Friedlander
-%   See the file COPYING.txt for full copyright information.
-%   Use the command 'spot.gpl' to locate this file.
-
-%   http://www.cs.ubc.ca/labs/scl/spot
 
 if length(s) > 1
-   result = x;
-   for i=1:length(s)
-      if iscell(result)
-         if strcmp(s(i).type,'{}')
-            result = builtin('subsref',result,s(i));
-         else
-            % Apply the subsref to each element
-            newresult = cell(1,length(result));
-            for j=1:length(result)
-               newresult{j} = subsref(result{j},s(i));
-            end
-            result = newresult;
-         end
-      else
-         result = subsref(result,s(i));
-      end
-   end
-   
-   if nargout > 1
-      for i=2:nargout
-         varargout{i} = [];
-      end
-   end
-   varargout{1} = result;
-   
-   return;
-end
-
-switch s.type
-   case {'.'}
-      % Set properties and flags
-      varargout{1} = x.(s.subs);
-
-   case {'{}'}
-      error('Cell-indexing is not supported.');
- 
-   case {'()'}
-      varargout{1} = subsref(double(x),s);
-
+    % Check for extended function calls
+    if strcmp(s(1).type,'.')
+        subs = [s.subs];
+        varargout{1} = feval(subs{1},x,subs{2:end});        
+    else
+        varargout = builtin('subsref',x,s);
+    %    result = x;
+    %    for i=1:length(s)
+    %       if iscell(result)
+    %          if strcmp(s(i).type,'{}')
+    %             result = builtin('subsref',result,s(i));
+    %          else
+    %             % Apply the subsref to each element
+    %             newresult = cell(1,length(result));
+    %             for j=1:length(result)
+    %                newresult{j} = subsref(result{j},s(i));
+    %             end
+    %             result = newresult;
+    %          end
+    %       else
+    %          result = subsref(result,s(i));
+    %       end
+    %    end
+    %
+    %    if nargout > 1
+    %       for i=2:nargout
+    %          varargout{i} = [];
+    %       end
+    %    end
+    %    varargout{1} = result;
+    %
+    %    return;
+    end
+    
+else
+    
+    switch s.type
+        case {'.'}
+            % Set properties and flags
+            varargout{1} = x.(s.subs);
+            
+        case {'{}'}
+            error('Cell-indexing is not supported.');
+            
+        case {'()'}
+            varargout{1} = subsref(double(x),s);
+            
+    end
 end
