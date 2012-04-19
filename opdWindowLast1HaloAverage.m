072cad8ba118ec7a0bf7a136c9e3c610d22d56bc
Henryk Modzelewski <henryk_modzelewski@mac.com>
2012-04-18 19:07:17 -0700

    added attribute help

--- opdWindowLast1HaloAverage.m
+++ opdWindowLast1HaloAverage.m
@@ -9,6 +9,12 @@ classdef opdWindowLast1HaloAverage < oppSpot
 %      P = number of processors
 %      H = half of the overlap's size
 %
+%   ATTRIBUTES:
+%      xshape = (p,3) vector holding start, size, end indecies
+%           of the default distribution of the input vector in every window
+%      yshape = (p,3) vector holding start, size, end indecies
+%           of the default distribution of the output vector in every window
+%
 %   Notes:
 %       1. This is a parallel/distributed operator
     
@@ -18,9 +24,9 @@ classdef opdWindowLast1HaloAverage < oppSpot
     properties (SetAccess = private)
         p = 0;
     l = 0;
-	h = 0;
-	yshape = 0;
-	xshape = 0;
+    h = 0;
+    yshape = 0;
+    xshape = 0;
     end
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -32,20 +38,20 @@ classdef opdWindowLast1HaloAverage < oppSpot
        % Constructor
        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
        function op = opdWindowLast1HaloAverage(varargin)
-	  assert(nargin==4,'dWindowLast1HaloAverage: wrong # of arguments')
+      assert(nargin==4,'dWindowLast1HaloAverage: wrong # of arguments')
       n = varargin{1};
-	  l = varargin{2};
-	  p = varargin{3};
-	  h = varargin{4};
+      l = varargin{2};
+      p = varargin{3};
+      h = varargin{4};
       assert(mod(n,l)==0,'Fatal error: L is not a valid last dimension')
       f = n/l;
           [ m xs ys ] = pSPOT.pWindow.funWindowLast1HaloShape( l, p, h );
-	  op = op@oppSpot('dWindowLast1HaloAverage',f*m,f*m);
-	  op.p = p;
+      op = op@oppSpot('dWindowLast1HaloAverage',f*m,f*m);
+      op.p = p;
       op.l = l;
-	  op.h = h;
-	  op.yshape = ys;
-	  op.xshape = xs;
+      op.h = h;
+      op.yshape = ys;
+      op.xshape = xs;
        end % function opdWindowLast1HaloAverage
        
        % utest ( skip dottest )
@@ -64,7 +70,7 @@ classdef opdWindowLast1HaloAverage < oppSpot
         % Multiplication
         function y = multiply(op,x,mode)
            if (mode == 1)
-	       y = pSPOT.pWindow.funWindowLast1HaloAverage(x,op.l,op.p,op.h);
+           y = pSPOT.pWindow.funWindowLast1HaloAverage(x,op.l,op.p,op.h);
            else
             error('No transpose/inverse defined')
            end
--- oplWindow1Dtpr.m
+++ oplWindow1Dtpr.m
@@ -8,6 +8,14 @@ classdef oplWindow1Dtpr < opSpot
 %      P = number of processors
 %      H = half of the overlap's size
 %
+%   ATTRIBUTES:
+%      oshape = (p,3) vector holding start, size, end indecies
+%           of the default distribution of the input vector in every window
+%      yshape = (p,3) vector holding start, size, end indecies
+%           of the default distribution of the output vector in every window
+%      xshape = (p,3) vector holding start, size, end indecies
+%           of the input vector that will end up in every ouput window
+%
 %   Notes:
 %       1. This is not a parallel/distributed operator
     
@@ -16,10 +24,10 @@ classdef oplWindow1Dtpr < opSpot
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     properties (SetAccess = private)
         p = 0;
-	h = 0;
-	oshape = 0;
-	yshape = 0;
-	xshape = 0;
+    h = 0;
+    oshape = 0;
+    yshape = 0;
+    xshape = 0;
     end
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -31,31 +39,31 @@ classdef oplWindow1Dtpr < opSpot
        % Constructor
        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
        function op = oplWindow1Dtpr(varargin)
-	  assert(nargin==3,'lWindow1Dtpr: wrong # of arguments')
-	  n = varargin{1};
-	  p = varargin{2};
-	  h = varargin{3};
+      assert(nargin==3,'lWindow1Dtpr: wrong # of arguments')
+      n = varargin{1};
+      p = varargin{2};
+      h = varargin{3};
           [ m os ys xs ] = pSPOT.pWindow.funWindowShape1D( n, p, h );
-	  op = op@opSpot('lWindow1Dtpr',m,n);
-	  op.p = p;
-	  op.h = h;
-	  op.oshape = os;
-	  op.yshape = ys;
-	  op.xshape = xs;
+      op = op@opSpot('lWindow1Dtpr',m,n);
+      op.p = p;
+      op.h = h;
+      op.oshape = os;
+      op.yshape = ys;
+      op.xshape = xs;
        end % function oplWindow1Dtpr
        
        % xtratests
        function result = xtratests(op)
            T = 14;
-	   x0=rand(op.n,1);
-	   y=op*x0;
-	   x1=op'*y;
-	   check=norm(x1-x0);
-	   if check < op.n*10^-T
+       x0=rand(op.n,1);
+       y=op*x0;
+       x1=op'*y;
+       check=norm(x1-x0);
+       if check < op.n*10^-T
                result = true;
-	   else
+       else
                result = false;
-	   end
+       end
        end % xtratests
 
     end % Methods
@@ -68,10 +76,10 @@ classdef oplWindow1Dtpr < opSpot
         % Multiplication
         function y = multiply(op,x,mode)
            if (mode == 1)
-	      [ A ] = pSPOT.pWindow.funWindow1DtprFor(op.n,op.p,op.h);
+          [ A ] = pSPOT.pWindow.funWindow1DtprFor(op.n,op.p,op.h);
               y = A * x;
            else
-	      [ B ] = pSPOT.pWindow.funWindow1DtprBck(op.n,op.p,op.h);
+          [ B ] = pSPOT.pWindow.funWindow1DtprBck(op.n,op.p,op.h);
               y = B * x;
            end
         end % Multipy
