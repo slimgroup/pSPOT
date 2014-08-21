68d7630aa42d929888472c1062f8be81c75b4b56
Daniel Hsieh <danielcdcj@yahoo.com.tw>
2014-08-21 14:31:01 -0700

    matlabpool and isdistributed path

--- isdistributed.m
+++ isdistributed.m
@@ -1,16 +1,20 @@
 function tf = isdistributed( x )
 %ISDISTRIBUTED Summary of this function goes here
 %   Detailed explanation goes here
-pSpotPath = pSPOT.path;
+isdistributedPaths = which('isdistributed','-ALL');
+builtInIsdistributedPath = isdistributedPaths{end};
+builtInIsdistributedPath(length(builtInIsdistributedPath)-15:length(builtInIsdistributedPath)) = [];
+newIsdistributedPath = isdistributedPaths{1};
+newIsdistributedPath(length(newIsdistributedPath)-15:length(newIsdistributedPath)) = [];
 % Grab names of installed toolboxes
 v = ver;
 [inst{1:length(v)}] = deal(v.Name);
 
 if ismember('Parallel Computing Toolbox', inst) % parallel installed
-    rmpath(pSpotPath)
+    addpath(builtInIsdistributedPath);
     %tf = builtin(isdistributed, x);
     tf = isdistributed(x);
-    addpath(pSpotPath)
+    addpath(newIsdistributedPath);
 else
     warning(['Parallel Computing Toolbox not found,'...
         ' using serial version of function']);
--- matlabpool.m
+++ matlabpool.m
@@ -1,16 +1,20 @@
 function varargout = matlabpool( varargin )
 %MATLABPOOL Summary of this function goes here
 %   Detailed explanation goes here
-pSpotPath = pSPOT.path;
+matlabpoolPaths = which('matlabpool','-ALL');
+builtInMatlabpoolPath = matlabpoolPaths{end};
+builtInMatlabpoolPath(length(builtInMatlabpoolPath)-12:length(builtInMatlabpoolPath)) = [];
+newMatlabpoolPath = matlabpoolPaths{1};
+newMatlabpoolPath(length(newMatlabpoolPath)-12:length(newMatlabpoolPath)) = [];
 % Grab names of installed toolboxes
 v = ver;
 [inst{1:length(v)}] = deal(v.Name);
 
 if ismember('Parallel Computing Toolbox', inst) % parallel installed
-    rmpath(pSpotPath)
+    addpath(builtInMatlabpoolPath);
     %[varargout{1:nargout}] = builtin('matlabpool', varargin{:});
     [varargout{1:nargout}] = matlabpool(varargin{:});
-    addpath(pSpotPath)
+    addpath(newMatlabpoolPath);
 else
     warning(['Parallel Computing Toolbox not found,'...
         ' using serial version of function']);
