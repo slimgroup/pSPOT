a7a7202639bbcf90560a83fecd7fff46281b6175
Daniel Hsieh <danielcdcj@yahoo.com.tw>
2014-08-20 17:05:10 -0700

    pSpot path

--- isdistributed.m
+++ isdistributed.m
@@ -1,16 +1,16 @@
 function tf = isdistributed( x )
 %ISDISTRIBUTED Summary of this function goes here
 %   Detailed explanation goes here
-slimapps = getenv('SLIM_APPS');
+pSpotPath = pSPOT.path;
 % Grab names of installed toolboxes
 v = ver;
 [inst{1:length(v)}] = deal(v.Name);
 
 if ismember('Parallel Computing Toolbox', inst) % parallel installed
-    rmpath([slimapps '/tools/utilities/pSPOT'])
+    rmpath(pSpotPath)
     %tf = builtin(isdistributed, x);
     tf = isdistributed(x);
-    addpath([slimapps '/tools/utilities/pSPOT'])
+    addpath(pSpotPath)
 else
     warning(['Parallel Computing Toolbox not found,'...
         ' using serial version of function']);
--- matlabpool.m
+++ matlabpool.m
@@ -1,16 +1,16 @@
 function varargout = matlabpool( varargin )
 %MATLABPOOL Summary of this function goes here
 %   Detailed explanation goes here
-slimapps = getenv('SLIM_APPS');
+pSpotPath = pSPOT.path;
 % Grab names of installed toolboxes
 v = ver;
 [inst{1:length(v)}] = deal(v.Name);
 
 if ismember('Parallel Computing Toolbox', inst) % parallel installed
-    rmpath([slimapps '/tools/utilities/pSPOT/'])
+    rmpath(pSpotPath)
     %[varargout{1:nargout}] = builtin('matlabpool', varargin{:});
     [varargout{1:nargout}] = matlabpool(varargin{:});
-    addpath([slimapps '/tools/utilities/pSPOT/'])
+    addpath(pSpotPath)
 else
     warning(['Parallel Computing Toolbox not found,'...
         ' using serial version of function']);
