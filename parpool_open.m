979d3118dd6469c510d7f67a8aaad24c2c9b2837
Henryk Modzelewski <slim@slim-omega>
2017-04-05 17:32:08 -0700

    removed back-compatiblity for parpool_*

--- parpool_open.m
+++ parpool_open.m
@@ -3,12 +3,5 @@ function pool = parpool_open(nw)
 % parpool_open returns empty 'N/A' if matalbppol is called
 % or parallel-pool object if parpool is called
 
-dcv=ver('distcomp');
-if str2double(dcv.Version) < 6.3
-    matlabpool('open',nw);
-    pool = 'N/A';
-else
     pool = parpool(nw);
-end
-
 end
